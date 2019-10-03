a5ff557d1773a0cba3fe01607bcd6c796b32f984
Sean Hammond <git@snhmnd.fastmail.com>
2019-10-03 18:31:40 +0100

    Add `make devdata`
    Also upgrade to Python 3 since the tox-envfile plugin requires it.

---
+++ bin/devdata.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import pathlib
+import shutil
+import subprocess
+import tempfile
+
+
+def main():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # The directory that we'll clone the devdata git repo into.
+        git_dir = os.path.join(tmpdirname, "devdata")
+
+        subprocess.check_call(
+            ["git", "clone", "git@github.com:hypothesis/devdata.git", git_dir]
+        )
+
+        # Copy devdata env file into place.
+        shutil.copyfile(
+            os.path.join(git_dir, "publisher-account-test-site", "devdata.env"),
+            os.path.join(pathlib.Path(__file__).parent.parent, ".devdata.env"),
+        )
+
+
+if __name__ == "__main__":
+    main()
